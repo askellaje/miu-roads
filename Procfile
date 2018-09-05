cb19f2b5eae7b56f8ff30e997451e098d4f3715b
Dmytro Ostapchuk <ostapchukvox@gmail.com>
2018-09-05 16:48:52 +0300

    delete wakeup

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn roads:app
-worker: python wakeup.py
--- wakeup.py
+++
@@ -1,8 +0,0 @@
-import requests
-import time
-
-# request the app every 5 mins
-while True:
-    req = requests.get('http://miu-roads.herokuapp.com/')
-    print (req.status_code)
-    time.sleep(60 * 5)
